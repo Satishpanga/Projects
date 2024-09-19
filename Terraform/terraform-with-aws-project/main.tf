ec0c2515fc54ac3fb06e3f3c1564eb1e43936c2c
Satishpanga <107531981+Satishpanga@users.noreply.github.com>
2024-09-19 23:14:58 +0530

    Update main.tf

--- Terraform/terraform-with-aws-project/main.tf
+++ Terraform/terraform-with-aws-project/main.tf
@@ -1,3 +1,5 @@
+#terraform init
+#terraform validate
 resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
 }
@@ -90,6 +92,8 @@ resource "aws_instance" "webserver2" {
   subnet_id              = aws_subnet.sub2.id
   user_data              = base64encode(file("userdata1.sh"))
 }
+# terraform plan 
+# terraform apply
 
 #create alb
 resource "aws_lb" "myalb" {
@@ -106,6 +110,7 @@ resource "aws_lb" "myalb" {
   }
 }
 
+#Creating target grops
 resource "aws_lb_target_group" "tg" {
   name     = "myTG"
   port     = 80
@@ -118,6 +123,7 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
+#Attaching Instances with the target group
 resource "aws_lb_target_group_attachment" "attach1" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.webserver1.id
@@ -130,6 +136,7 @@ resource "aws_lb_target_group_attachment" "attach2" {
   port             = 80
 }
 
+#Adding a Listener for the communication between the Instances and the loadbalancer
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.myalb.arn
   port              = 80
@@ -144,3 +151,8 @@ resource "aws_lb_listener" "listener" {
 output "loadbalancerdns" {
   value = aws_lb.myalb.dns_name
 }
+
+#terraform validate
+#terraform fmt
+#terraform plan
+#Terraform apply -auto-approve
